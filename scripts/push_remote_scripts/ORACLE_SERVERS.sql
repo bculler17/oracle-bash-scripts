cdfb7e59e0b0886c1d4670a15e6ba5c3ea2e57a8
Beth Culler <49499193+bculler17@users.noreply.github.com>
2022-01-20 14:20:13 -0600

    Update ORACLE_SERVERS.sql

--- scripts/push_remote_scripts/ORACLE_SERVERS.sql
+++ scripts/push_remote_scripts/ORACLE_SERVERS.sql
@@ -1,18 +1,18 @@
 create table ORACLE_SERVERS (
-  ID NUMBER,
-  HOSTNAME VARCHAR2(35) not null unique,
-  IPADDRESS VARCHAR2(15) not null unique,
-  OS_VERSION VARCHAR2(100),
-  UP_TIME VARCHAR2(35),
-  FUNCTION VARCHAR2(25) CHECK (function='PROD' OR function='DEV/TEST'),
-  STATUS VARCHAR2(10) not null CHECK (status='ACTIVE' OR status='INACTIVE' OR status='TEST'),
-  ONLINE_STATUS VARCHAR2(10),
-  ALERT_STATUS VARCHAR2(10) CHECK (alert_status='YES' OR alert_status='NO'),
-  RECORD_CREATED DATE default SYSDATE,
+  	ID NUMBER,
+  	HOSTNAME VARCHAR2(35) not null unique,
+  	IPADDRESS VARCHAR2(15) not null unique,
+  	OS_VERSION VARCHAR2(100),
+  	UP_TIME VARCHAR2(35),
+  	FUNCTION VARCHAR2(25) CHECK (function='PROD' OR function='DEV/TEST'),
+  	STATUS VARCHAR2(10) not null CHECK (status='ACTIVE' OR status='INACTIVE' OR status='TEST'),
+  	ONLINE_STATUS VARCHAR2(10),
+  	ALERT_STATUS VARCHAR2(10) CHECK (alert_status='YES' OR alert_status='NO'),
+  	RECORD_CREATED DATE default SYSDATE,
 	RECORD_CREATOR VARCHAR2(25) not null,
 	RECORD_UPDATED DATE,
 	RECORD_UPDATED_BY VARCHAR2(1000),
-  constraint oracle_servers_PK PRIMARY KEY(id)
+  	constraint oracle_servers_PK PRIMARY KEY(id)
 );
 -- WHERE os_version =  will be autopopulated by my it_reaches_out.sh script (the current version of the operating system used by the database server)
 -- up_time = will be autopopulated by my it_reaches_out.sh script (the number of days the server has been online)
